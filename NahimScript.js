290fb814032be1457cd08422bbd5f0bdf6e42368
w0rzz <35713699+w0rzz@users.noreply.github.com>
2020-06-05 23:33:27 +0100

    Update NahimScript.js

--- NahimScript.js
+++ NahimScript.js
@@ -1,11 +1,11 @@
 
-var fullname = "Nahim Adebisi";
+var fullName = "Nahim Adebisi";
 var id = "HNG-06093";
 var progLang = "JavaScript";
 var email = "nahimadebisi@gmail.com";
 
 function displayMessage() {
-    console.log('Hello World, this is ${fullname} with HNGi7 ID ${id} using ${progLang} for stage 2 task.${email}');
+    console.log('Hello World, this is ' + fullName + 'with HNGi7 ID ' + id + 'using ' + progLang + 'for stage 2 task.');
 }
 
 displayMessage();
